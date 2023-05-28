652ac9605bd0163694582c8fa3d65149cfc015c6
qq_41093957 <qq_41093957@gitcode.net>
2023-05-28 15:18:40 +0800

    first commit

---
+++ MBIPED/main.py
@@ -0,0 +1,20 @@
+""" BIPED dataset augmentation processes
+
+This script has the whole augmentation methods described in the paper entitle
+Dense Extreme Inception Network: Towards a Robust CNN Model for Edge Detection *WACV2020*
+which can be download in: https://arxiv.org/pdf/1909.01955.pdf
+"""
+from data_augmentation import augment_data
+
+def main(dataset_dir):
+    augment_both = True  # to augment the RGB and target (edge_map) image at the same time
+    augment_data(base_dir=dataset_dir, augment_both=augment_both, use_all_type=True)
+
+
+if __name__=='__main__':
+     # Once the BIPED datset is downloaded, put the localization of the dataset
+     # for example if the data is in /home/user_name/datasets/BIPED
+    #  put /home/home/user_name
+    dataset_dir = 'data/BIPEDv2'
+    augment_both = True  # to augment the RGB and target (edge_map) image at the same time
+    augment_data(base_dir=dataset_dir, augment_both=True, use_all_type=True)
---
+++ run.sh
@@ -0,0 +1,8 @@
+#* 数据增强
+# python MBIPED/main.py
+
+#* 测试，对data中的图片进行处理
+# python main.py --choose_test_data=-1
+
+#* 训练
+python main.py --is_testing=0
